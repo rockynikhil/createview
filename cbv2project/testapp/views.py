d4d37745c4a00ffacc19279749e15625fa92e177
nikhileswarsharma <eswarnikhil78@gmail.com>
2023-05-25 17:08:46 +0530

    first ommit by eswarnikhil

---
+++ cbv2project/testapp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from testapp.models import Book
+from  django.views.generic import ListView,DetailView,CreateView,DeleteView,UpdateView
+
+
+# Create your views here.
+class BookListView(ListView):
+    model = Book
+   #template_name = 'testapp/book.html'
+    #context_object_name = 'books'
+
+class BookListView2(ListView):
+    model = Book
+    template_name = 'testapp/book.html'
+    context_object_name = 'books'
+
+class BookDetailView(DetailView):
+    model = Book
+    #template_name = 'testapp/book_detail.html'
+    #context_object_name = 'books'
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = '__all__'
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = '__all__'
+
+
+from django.urls import  reverse_lazy
+class BookDeleteView(DeleteView):
+    model = Book
+    success_url = reverse_lazy('listbooks')
+
+
